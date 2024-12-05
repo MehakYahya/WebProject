45dbe1e672739c602fe0234f413cb7734d5cac2f
Samaha Munir <samimunir196@gmail.com>
2024-12-05 21:35:46 +0500

    add app

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+import {FormsModule} from '@angular/forms';
+
+import {ProductsComponent} from './pages/products/products.component';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [
+
+    FormsModule,
+
+    ProductsComponent,
+
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'WebProject';
+}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+import {ProductsComponent} from './pages/products/products.component';
+import {CreateOrderComponent} from './pages/create-order/create-order.component';
+import {MyOrderComponent} from './pages/my-order/my-order.component';
+
+export const routes: Routes = [
+  {
+    path:'',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component:ProductsComponent
+
+  },
+  {
+    path: 'create-order',
+    component:CreateOrderComponent
+  },
+  {
+    path: 'my-order',
+    component:MyOrderComponent
+  }
+];
