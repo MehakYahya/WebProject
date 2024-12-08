8cce250cc2ed8d645ad349dcd28905aee7edfa7f
syeda hira <114241999+hira524@users.noreply.github.com>
2024-12-08 17:46:35 +0500

    admin-dashboard

---
+++ admin-dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-admin-dashboard',
+  templateUrl: './admin-dashboard.component.html',
+  styleUrls: ['./admin-dashboard.component.css'],
+  imports: [
+    RouterLink
+  ]
+})
+export class AdminDashboardComponent {
+  title = 'Admin Dashboard';
+}
