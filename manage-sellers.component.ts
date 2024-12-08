3e52d8df9636f435dcd79f742c3bc79cf0853b65
syeda hira <114241999+hira524@users.noreply.github.com>
2024-12-08 17:48:27 +0500

    manage-sellers

---
+++ manage-sellers.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-manage-sellers',
+  templateUrl: './manage-sellers.component.html',
+  styleUrls: ['./manage-sellers.component.css'],
+  imports: [
+
+  ]
+})
+export class ManageSellersComponent {
+
+}
