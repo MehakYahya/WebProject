6fcd02b4b899c6812a4776e5b0e2343e734928d0
Mehak Yahya <168831879+MehakYahya@users.noreply.github.com>
2024-12-11 14:51:58 +0500

    View Product

---
+++ src/app/view-product/view-product.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import {NgForOf, NgIf} from '@angular/common'; // Import the service
+
+@Component({
+  selector: 'app-view-product',
+  templateUrl: './view-product.component.html',
+  styleUrls: ['./view-product.component.css'],
+  imports: [
+    NgForOf,
+    NgIf
+  ]
+})
+export class ViewProductComponent implements OnInit {
+  products: { name: string; price: number; image: string; description: string; quantity: number }[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+
+    this.productService.products$.subscribe((products) => {
+      this.products = products;
+    });
+  }
+}
+
