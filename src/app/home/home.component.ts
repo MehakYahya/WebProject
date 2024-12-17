cf907988e85be6efffc53c623ed4029893c0fbb2
Mehak Yahya <168831879+MehakYahya@users.noreply.github.com>
2024-12-18 02:13:57 +0500

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,68 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderService } from '../order.service';
-import { NgForOf, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  imports: [NgIf, NgForOf],
   styleUrls: ['./home.component.css'],
+  standalone: true
 })
-export class HomeComponent implements OnInit {
-  pendingOrders: any[] = [];
-  unpaidOrders: any[] = [];
-  toBeReviewedOrders: any[] = [];
-  showAllPendingOrders: boolean = false; // Flag to control 'See More'
+export class HomeComponent implements OnInit{
+  totalOrders = 0;
+  deliveredOrders = 0;
+  pendingOrders = 0;
 
   constructor(private orderService: OrderService) {}
 
-  ngOnInit() {
-    // Subscribe to the orders Observable to get real-time updates
-    this.orderService.pendingOrders$.subscribe((orders) => {
-      this.pendingOrders = orders;
-    });
-
-    this.orderService.unpaidOrders$.subscribe((orders) => {
-      this.unpaidOrders = orders;
-    });
-
-    this.orderService.toBeReviewedOrders$.subscribe((orders) => {
-      this.toBeReviewedOrders = orders;
-    });
-  }
-
-  // Toggle "See More" functionality for pending orders
-  toggleSeeMore() {
-    this.showAllPendingOrders = !this.showAllPendingOrders;
-  }
-
-  // Mark an order as To Be Reviewed
-  markAsToBeReviewed(orderNumber: string) {
-    const order = this.pendingOrders.find((o) => o.orderNumber === orderNumber);
-    if (order) {
-      // Remove from pending orders and add to toBeReviewed orders
-      this.pendingOrders = this.pendingOrders.filter(
-          (o) => o.orderNumber !== orderNumber
-      );
-      this.toBeReviewedOrders.push({ ...order, status: 'To Be Reviewed' });
-
-      // Call the service to update the order status
-      this.orderService.updateOrderStatus(orderNumber, 'toBeReviewed');
-    }
-  }
-
-  // Mark an order as Unpaid
-  markAsUnpaid(orderNumber: string) {
-    const order = this.pendingOrders.find((o) => o.orderNumber === orderNumber);
-    if (order) {
-      // Remove from pending orders and add to unpaid orders
-      this.pendingOrders = this.pendingOrders.filter(
-          (o) => o.orderNumber !== orderNumber
-      );
-      this.unpaidOrders.push({ ...order, status: 'Unpaid' });
-
-      // Call the service to update the order status
-      this.orderService.updateOrderStatus(orderNumber, 'unpaid');
-    }
+  ngOnInit(): void {
+    this.orderService.totalOrders$.subscribe(count => (this.totalOrders = count));
+    this.orderService.deliveredOrders$.subscribe(count => (this.deliveredOrders = count));
+    this.orderService.pendingOrders$.subscribe(count => (this.pendingOrders = count));
   }
 }
