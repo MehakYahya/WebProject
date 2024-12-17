794716483fc52066f135b66f5386299f8ac3b1cb
Mehak Yahya <168831879+MehakYahya@users.noreply.github.com>
2024-12-18 00:51:19 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import {HttpClientModule, provideHttpClient} from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes), // Provides routing for the application
-    provideHttpClient()    // Provides HTTP client functionality
+    provideRouter(routes),
+    provideHttpClient(),
+    HttpClientModule,
   ],
 }).catch((err) => console.error(err));
